c0a8b4d430130f872f50aac795f974725841d305
Justin Sabath <justin.sabath@aol.com>
2020-01-27 20:18:38 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,25 +43,31 @@ def naming_system
   # HoH all the way to Species!
 
      {
-        label: "Kingdom",
-        sub_category: {
-            label: "Phylum",
-            sub_category: {
-                label: "Class",
-                sub_category: {
-                    label: "Order",
-                    sub_category: {
-                        label: "Family",
-                        sub_category: {
-                            label: "Genus",
-                            sub_category: {
-                                label: "Species",
-                                sub_category: nil
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
+       label: "Kingdom",
+       sub_category: {
+         label: "Phylum",
+         sub_category: {
+           label: "Class",
+           sub_category: {
+             label: "Order",
+             sub_category: {
+               label: "Family",
+               sub_category: {
+                 label: "Genus",
+                 sub_category: {
+                   lab"Species",
+                   sub_category: nil
+                 }
+                 
+               }
+               
+             }
+             
+           }
+           
+         }
+         
+         
+       }
+       
+     }}
