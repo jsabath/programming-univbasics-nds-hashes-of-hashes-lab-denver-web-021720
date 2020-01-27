0162b80551c49e0098154b258daf962257a3d068
Justin Sabath <justin.sabath@aol.com>
2020-01-27 20:28:50 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -56,11 +56,5 @@ def naming_system
                  label: "Genus",
                  sub_category: {
                    label: "Species",
-                   sub_category: nil 
-                 }
-               }
-             }
-           }
-         }
-     }
+                   sub_category: nil }}}}}}
 end
